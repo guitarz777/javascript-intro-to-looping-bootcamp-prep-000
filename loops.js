8404609987be03e9ed4fb6b19d11854bfc277ecf
guitarz777@gmail.com <guitarz777@gmail.com>
2018-09-03 21:32:48 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,11 @@
+function forLoop(array){
+  
+  for (i=1; i<26; i++){
+    if (i===1){
+      array.push(`I am ${i} strange loop.`);
+    }else{
+    array.push(`I am ${i} strange loops.`);
+  }
+  }
+  return array
+}
