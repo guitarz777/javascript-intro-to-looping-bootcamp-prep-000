9fbf8db6e260a8f73a2d5655e56fca46d8ebbb82
guitarz777@gmail.com <guitarz777@gmail.com>
2018-09-03 21:48:59 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,9 +2,9 @@ function forLoop(array){
   
   for (i=0; i<25; i++){
     if (i===1){
-      array.push(`I am 1 strange loop.`);
+      array.unshift(`I am 1 strange loop.`);
     }else{
-    array.push(`I am ${i} strange loops.`);
+    array.unshift(`I am ${i} strange loops.`);
     }
   }
 }
